6e14e7687e463cfb3d75b45c0e43817523722795
Jokul482 <45899547+Jokul482@users.noreply.github.com>
2024-08-26 17:52:14 +0800

    update config file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,12 +6,7 @@ export default defineUserConfig({
     lang: 'zh-CN',
     title: 'Jokul',
     theme: plumeTheme({
-        profile: {
-            name: 'Jokul',
-            description: '「探索生活 记录旅程」',
-            avatar: 'https://avatars.githubusercontent.com/u/45899547?v=4',
-            circle: true, // 是否为圆形头像
-        }
+
     }),
     bundler: viteBundler(),
 })
